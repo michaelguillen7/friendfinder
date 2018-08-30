ede0a50fc1937888c386d0fa1c21ec41950fa550
Michael Guillen <michaelguillen@Michaels-MacBook-Pro.local>
2018-08-30 14:07:35 -0500

    added data folder and route

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+
+friendData = [
+    {
+        name: "Michael Guillen",
+        photoURL: "Your Photo",
+        scores: ["1","2","3","5","5","4","3","2","1","2"]
+        
+    }
+];
+
+module.exports = friendData;
+
+
+
